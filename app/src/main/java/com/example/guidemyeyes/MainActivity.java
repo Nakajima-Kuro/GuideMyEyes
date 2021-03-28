8630bf52699445fc3c90586971099c2dd35f54a7
Nam Anh <tnnamanh@gmail.com>
2021-03-28 18:51:03 +0700

    Add navigation between fragments

--- app/src/main/java/com/example/guidemyeyes/MainActivity.java
+++ app/src/main/java/com/example/guidemyeyes/MainActivity.java
@@ -1,8 +1,19 @@
 package com.example.guidemyeyes;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.NavDirections;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +22,35 @@ public class MainActivity extends AppCompatActivity {
         setTheme(R.style.Theme_GuideMyEyes);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Declare Fragments
+        HomeFragment homeFragment = new HomeFragment();
+        SettingsFragment settingsFragment = new SettingsFragment();
+        GuideFragment guideFragment = new GuideFragment();
+
+        FragmentTransaction fragmentTransaction = getSupportFragmentManager().beginTransaction();
+        fragmentTransaction.replace(R.id.fragment_frame_layout, homeFragment);
+        fragmentTransaction.addToBackStack(null);
+        fragmentTransaction.commit();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(
+                item -> {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.menuHome: {
+                            selectedFragment = homeFragment;
+                            break;
+                        }
+                        case R.id.menuSetting: {
+                            selectedFragment = settingsFragment;
+                            break;
+                        }
+                    }
+                    if (selectedFragment != null && !selectedFragment.isVisible()) {
+                        getSupportFragmentManager().beginTransaction().addToBackStack(null).replace(R.id.fragment_frame_layout,selectedFragment).commit();
+                    }
+                    return true;
+                });
     }
 }
--- app/src/main/java/com/example/guidemyeyes/SettingsFragment.java
+++ app/src/main/java/com/example/guidemyeyes/SettingsFragment.java
@@ -3,36 +3,33 @@ package com.example.guidemyeyes;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.preference.PreferenceFragmentCompat;
 import androidx.transition.TransitionInflater;
 
+import android.preference.PreferenceFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class HomeFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
 
-    public HomeFragment() {
+    public SettingsFragment() {
         // Required empty public constructor
     }
 
-    public static HomeFragment newInstance(String param1, String param2) {
-        HomeFragment fragment = new HomeFragment();
-        return fragment;
+    @Override
+    public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        setPreferencesFromResource(R.xml.preferences, rootKey);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         TransitionInflater inflater = TransitionInflater.from(requireContext());
-        setEnterTransition(inflater.inflateTransition(R.transition.slide_left));
+        setEnterTransition(inflater.inflateTransition(R.transition.slide_right));
+        setExitTransition(inflater.inflateTransition(R.transition.slide_right));
         setAllowEnterTransitionOverlap(true);
         setAllowReturnTransitionOverlap(true);
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
-    }
 }
